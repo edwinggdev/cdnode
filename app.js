2ff3ca011561cb239505cb394e20f297b09da68b
edwingarciagaitan82 <edwingarciagaitan82@gmail.com>
2024-09-06 20:36:44 -0500

    edu express heroku

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const puerto = process.env.PORT || 3000
+
+app.get("/", (req,res) =>{
+    res.send("Hola Mundo")
+} )
+app.get("/nombre", (req,res) =>{
+    res.send("Talento Tech 2024")
+})
+app.listen(puerto, ()=>{ console.log("servidor express en puerto ", puerto)  } )
