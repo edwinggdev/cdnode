71f397619ae0647fabf1241c1b5057508e04933f
edwinggdev <edwin_garcia5@hotmail.com>
2024-11-14 22:22:13 -0500

    test

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const puerto = process.env.PORT || 300
+const puerto = process.env.PORT || 3000
 
 app.get("/", (req,res) =>{
     res.send("Hola Mundo !!!")
